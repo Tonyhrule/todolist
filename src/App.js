e6726a94323b883333b4d80d8b9a5ced41559447
Tony <Tonyhrule.com>
2021-06-21 17:23:50 -0400

    e

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+      inputValue: ""
+    };
+    this.loadData = this.loadData.bind(this);
+    this.delete = this.delete.bind(this);
+    this.add = this.add.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+  }
+  loadData() {
+    return this.state.list.map((data, index) => {
+      return (
+        <div className="item">
+          <p>{data.data}</p>
+          <button index={index} className="delete" onClick={this.delete}>X</button>
+          <input type="checkbox" checked={data.checked} onChange={function(i){
+            let temp = this.state.list.concat();
+            if (temp[i].checked) temp[i].checked = false;
+            else temp[i].checked = true;
+            this.setState({ list: temp });
+          }.bind(this, index)} />
+        </div>
+      );
+    });
+  }
+  add() {
+    this.setState({
+      list: this.state.list.concat({data: this.state.inputValue, checked: false})
+    });
+  }
+  delete(e) {
+    let index = Number(e.target.getAttribute("index"));
+    let temp = this.state.list.concat();
+    temp.splice(index, 1);
+    this.setState({
+      list: temp
+    });
+  }
+  updateInput(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  }
+  render() {
+    return (
+      <div id="App">
+        <input onChange={this.updateInput} />
+        <button onClick={this.add}>Add</button>
+        <div id="list">
+          {this.loadData()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
